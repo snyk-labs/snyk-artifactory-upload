c7330bffb95775ca3a3fc2f36b584b7ddae089fa
olegshprengelsnyk <oleg.shprengel@snyk.io>
2023-12-04 10:42:55 -0700

    scaffolding in place

---
+++ index.js
@@ -0,0 +1,24 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const tl = require("azure-pipelines-task-lib/task");
+async function run() {
+    try {
+        const inputString = tl.getInput('samplestring', true);
+        if (inputString === 'bad') {
+            tl.setResult(tl.TaskResult.Failed, 'Bad input was given');
+            return;
+        }
+        console.log('Hello', inputString);
+    }
+    catch (err) {
+        if (err instanceof Error) {
+            tl.setResult(tl.TaskResult.Failed, err.message);
+        }
+        else {
+            // Handle other cases where 'err' might not be an error object
+            console.error('An unknown error occurred:', err);
+            tl.setResult(tl.TaskResult.Failed, 'An unknown error occurred');
+        }
+    }
+}
+run();

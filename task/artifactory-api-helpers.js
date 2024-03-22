20fed84e9f5c8dfcd7a825171b69e9034ffa1d5e
Nathan Roys <nathan.roys@snyk.io>
2024-03-22 14:42:32 +0000

    .

--- task/artifactory-api-helpers.js
+++ task/artifactory-api-helpers.js
@@ -1,127 +1,135 @@
 "use strict";
-var __assign = (this && this.__assign) || function () {
-    __assign = Object.assign || function(t) {
-        for (var s, i = 1, n = arguments.length; i < n; i++) {
-            s = arguments[i];
-            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
-                t[p] = s[p];
-        }
-        return t;
-    };
-    return __assign.apply(this, arguments);
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
 };
-exports.__esModule = true;
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
 exports.setProperties = void 0;
-var tl = require("azure-pipelines-task-lib/task");
-var axios_1 = require("axios");
-var axios_retry_1 = require("axios-retry");
-var Utils = require("./helpers");
-(0, axios_retry_1["default"])(axios_1["default"], {
+const tl = require("azure-pipelines-task-lib/task");
+const axios_1 = __importDefault(require("axios"));
+const axios_retry_1 = __importDefault(require("axios-retry"));
+const Utils = __importStar(require("./helpers"));
+(0, axios_retry_1.default)(axios_1.default, {
     retries: 10,
-    retryDelay: axios_retry_1["default"].exponentialDelay,
-    onRetry: function (retryCount, error, Config) { console.log("Axios request failed with " + error + " retrying now.."); }
+    retryDelay: axios_retry_1.default.exponentialDelay,
+    onRetry: (retryCount, error, Config) => { console.log("Axios request failed with " + error + " retrying now.."); }
 });
 function setProperties(properties) {
     var _a, _b;
-    var inputType = (_a = tl.getInput("InputType", true)) === null || _a === void 0 ? void 0 : _a.toLowerCase();
+    const inputType = (_a = tl.getInput("InputType", true)) === null || _a === void 0 ? void 0 : _a.toLowerCase();
     // get username/password details from service connection
-    var serviceConnectionId = tl.getInput('artifactoryServiceConnection', true);
-    var auth = tl.getEndpointAuthorization(serviceConnectionId, false);
-    var authType = tl.getEndpointAuthorizationScheme(serviceConnectionId, false);
-    var authToken = '';
+    const serviceConnectionId = tl.getInput('artifactoryServiceConnection', true);
+    const auth = tl.getEndpointAuthorization(serviceConnectionId, false);
+    let authType = tl.getEndpointAuthorizationScheme(serviceConnectionId, false);
+    let authToken = '';
     if (authType == 'UsernamePassword') {
-        var username = auth === null || auth === void 0 ? void 0 : auth.parameters['username'];
-        var password = auth === null || auth === void 0 ? void 0 : auth.parameters['password'];
-        authToken = Buffer.from("".concat(username, ":").concat(password)).toString('base64');
+        const username = auth === null || auth === void 0 ? void 0 : auth.parameters['username'];
+        const password = auth === null || auth === void 0 ? void 0 : auth.parameters['password'];
+        authToken = Buffer.from(`${username}:${password}`).toString('base64');
         authType = 'Basic';
     }
     else if (authType == 'Token') {
         authToken = auth === null || auth === void 0 ? void 0 : auth.parameters['apitoken'];
         authType = 'Bearer';
     }
-    var baseUrl = tl.getEndpointUrl(serviceConnectionId, true);
+    const baseUrl = tl.getEndpointUrl(serviceConnectionId, true);
     //set API headers
-    var headers = {
-        Authorization: "".concat(authType, " ").concat(authToken),
-        'Content-Type': 'application/json'
+    const headers = {
+        Authorization: `${authType} ${authToken}`,
+        'Content-Type': 'application/json', // Set content type based on your requirements
     };
     //Retrieve artifact URLs
-    var artifactUrls = [];
+    let artifactUrls = [];
     if (inputType == "urllist") {
-        var delimiter = tl.getInput('delimiter', false) || ',';
+        const delimiter = tl.getInput('delimiter', false) || ',';
         artifactUrls = (_b = tl.getInput('artifactUrls', true)) === null || _b === void 0 ? void 0 : _b.split(delimiter);
-        var _loop_1 = function (artifactUrlShort) {
+        //add properties to each artifact
+        for (let artifactUrlShort of artifactUrls) {
             artifactUrlShort = Utils.encodeUrl(artifactUrlShort);
-            var artifactUrl = "".concat(baseUrl, "/api/storage/").concat(artifactUrlShort); // Construct the complete URL
-            Object.keys(properties).forEach(function (prop) {
-                var queryParams = {
-                    "properties": [prop] + '=' + properties[prop]
+            const artifactUrl = `${baseUrl}/api/storage/${artifactUrlShort}`; // Construct the complete URL
+            Object.keys(properties).forEach((prop) => {
+                const queryParams = {
+                    "properties": [prop] + '=' + properties[prop], // Assuming 'prop' and 'properties' are defined elsewhere
                 };
-                axios_1["default"].put(artifactUrl, null, {
+                axios_1.default.put(artifactUrl, null, {
                     params: queryParams,
-                    headers: headers
+                    headers: headers,
                 })
-                    .then(function (response) {
-                    console.log("Successfully set property '".concat(prop, "' on Artifact ").concat(artifactUrlShort));
-                })["catch"](function (error) {
+                    .then(response => {
+                    console.log(`Successfully set property '${prop}' on Artifact ${artifactUrlShort}`);
+                })
+                    .catch(error => {
                     //test
                     console.log('Error while attempting to add property to Artifact:' + error);
                     // Handle errors here
                     process.exit(1); // Exiting with a non-zero code indicating an error
                 });
             });
-        };
-        //add properties to each artifact
-        for (var _i = 0, artifactUrls_1 = artifactUrls; _i < artifactUrls_1.length; _i++) {
-            var artifactUrlShort = artifactUrls_1[_i];
-            _loop_1(artifactUrlShort);
         }
     }
     else if (inputType == "build") {
-        var buildName = tl.getInput('BuildName', true);
-        var buildNumber = tl.getInput('BuildNumber', true);
-        var projectName = tl.getInput('ProjectKey', true);
-        var BuildStatus = tl.getInput('BuildStatus', false);
-        var searchBody = __assign({ "buildName": buildName, "buildNumber": buildNumber, "project": projectName }, (BuildStatus !== null && { myProperty: BuildStatus }));
-        var searchUrl = "".concat(baseUrl, "/api/search/buildArtifacts");
-        axios_1["default"].post(searchUrl, JSON.stringify(searchBody), {
-            headers: headers
+        const buildName = tl.getInput('BuildName', true);
+        const buildNumber = tl.getInput('BuildNumber', true);
+        const projectName = tl.getInput('ProjectKey', true);
+        const BuildStatus = tl.getInput('BuildStatus', false);
+        const searchBody = Object.assign({ "buildName": buildName, "buildNumber": buildNumber, "project": projectName }, (BuildStatus !== null && { myProperty: BuildStatus }));
+        const searchUrl = `${baseUrl}/api/search/buildArtifacts`;
+        axios_1.default.post(searchUrl, JSON.stringify(searchBody), {
+            headers: headers,
         })
-            .then(function (response) {
+            .then((response) => {
             console.log("Data received from build search API: " + JSON.stringify(response.data));
-            artifactUrls = response.data.results.map(function (obj) {
-                var downloadUri = obj.downloadUri;
-                var trimmedUrl = downloadUri.replace("".concat(baseUrl, "/"), "");
+            artifactUrls = response.data.results.map((obj) => {
+                const { downloadUri } = obj;
+                const trimmedUrl = downloadUri.replace(`${baseUrl}/`, "");
                 return trimmedUrl;
             });
-            var _loop_2 = function (artifactUrlShort) {
+            for (let artifactUrlShort of artifactUrls) {
                 artifactUrlShort = Utils.encodeUrl(artifactUrlShort);
-                var artifactUrl = "".concat(baseUrl, "/api/storage/").concat(artifactUrlShort); // Construct the complete URL
-                Object.keys(properties).forEach(function (prop) {
-                    var queryParams = {
-                        "properties": [prop] + '=' + properties[prop]
+                const artifactUrl = `${baseUrl}/api/storage/${artifactUrlShort}`; // Construct the complete URL
+                Object.keys(properties).forEach((prop) => {
+                    const queryParams = {
+                        "properties": [prop] + '=' + properties[prop], // Assuming 'prop' and 'properties' are defined elsewhere
                     };
-                    setTimeout(function () {
-                        return axios_1["default"].put(artifactUrl, null, {
-                            params: queryParams,
-                            headers: headers
-                        })
-                            .then(function (response) {
-                            console.log("Successfully set property '".concat(prop, "' on Artifact ").concat(artifactUrlShort));
-                            // Adding a delay between each API call
-                        })["catch"](function (error) {
-                            console.log('Error while attempting to add property to Artifact: ' + error);
-                            // Handle errors here
-                            process.exit(1); // Exiting with a non-zero code indicating an error
-                        });
-                    }, 1000);
+                    setTimeout(() => axios_1.default.put(artifactUrl, null, {
+                        params: queryParams,
+                        headers: headers,
+                    })
+                        .then(response => {
+                        console.log(`Successfully set property '${prop}' on Artifact ${artifactUrlShort}`);
+                        // Adding a delay between each API call
+                    })
+                        .catch(error => {
+                        console.log('Error while attempting to add property to Artifact: ' + error);
+                        // Handle errors here
+                        process.exit(1); // Exiting with a non-zero code indicating an error
+                    }), 1000);
                 });
-            };
-            for (var _i = 0, artifactUrls_2 = artifactUrls; _i < artifactUrls_2.length; _i++) {
-                var artifactUrlShort = artifactUrls_2[_i];
-                _loop_2(artifactUrlShort);
             }
-        })["catch"](function (error) {
+        })
+            .catch((error) => {
             console.error('Error from Artifactory search builds API:', error.response ? error.response.data : error.message);
             process.exit(1);
         });
--- task/index.js
+++ task/index.js
@@ -1,4 +1,27 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -8,126 +31,91 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (_) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
-exports.__esModule = true;
-var tl = require("azure-pipelines-task-lib/task");
-var fs = require("fs");
-var Utils = require("./helpers");
-var Artifactory = require('./artifactory-api-helpers');
+Object.defineProperty(exports, "__esModule", { value: true });
+const tl = require("azure-pipelines-task-lib/task");
+const fs = __importStar(require("fs"));
+const Utils = __importStar(require("./helpers"));
+const Artifactory = require('./artifactory-api-helpers');
 function run() {
-    return __awaiter(this, void 0, void 0, function () {
-        var os, slash, operation, snykFilePath, fileLocation, scanData, codeJson, err_1, jsonFilePath, scanData, scanData_1;
-        return __generator(this, function (_a) {
-            switch (_a.label) {
-                case 0:
-                    os = process.env.AGENT_OS;
-                    if (os === 'Windows_NT') {
-                        slash = '\\'; // Backslash for Windows
-                    }
-                    else {
-                        slash = '/'; // Forward slash for Linux and macOS
-                    }
-                    operation = tl.getInput('Operation', true);
-                    snykFilePath = tl.getInput('SnykDirectory') + slash;
-                    if (!(operation == "Copy" || operation == "CopyAndProcess")) return [3 /*break*/, 7];
-                    fileLocation = "";
-                    try {
-                        console.log("Attempting to retrieve Snyk report file from location: " + snykFilePath);
-                        fileLocation = Utils.findLocalReportFile();
-                        if (fileLocation == null) {
-                            console.log("Failed to find Snyk report file");
-                            process.exit(1);
-                        }
-                    }
-                    catch (err) {
-                        console.log("Error retrieving Snyk report file: " + err);
-                        process.exit(1);
-                    }
-                    scanData = {};
-                    if (!fileLocation) return [3 /*break*/, 5];
-                    _a.label = 1;
-                case 1:
-                    _a.trys.push([1, 3, , 4]);
-                    return [4 /*yield*/, Utils.readFileContents(fileLocation)];
-                case 2:
-                    codeJson = _a.sent();
-                    scanData = Utils.processCode(codeJson);
-                    return [3 /*break*/, 4];
-                case 3:
-                    err_1 = _a.sent();
-                    console.log("Error processing code results: " + err_1);
+    return __awaiter(this, void 0, void 0, function* () {
+        //determine whether to use forward or backslash depending on OS
+        const os = process.env.AGENT_OS;
+        let slash;
+        if (os === 'Windows_NT') {
+            slash = '\\'; // Backslash for Windows
+        }
+        else {
+            slash = '/'; // Forward slash for Linux and macOS
+        }
+        //Get operation to be performed, can be copy, process or both
+        const operation = tl.getInput('Operation', true);
+        const snykFilePath = tl.getInput('SnykDirectory') + slash;
+        if (operation == "Copy" || operation == "CopyAndProcess") {
+            //get file location from file path directory
+            let fileLocation = "";
+            try {
+                console.log("Attempting to retrieve Snyk report file from location: " + snykFilePath);
+                fileLocation = Utils.findLocalReportFile();
+                if (fileLocation == null) {
+                    console.log("Failed to find Snyk report file");
                     process.exit(1);
-                    return [3 /*break*/, 4];
-                case 4: return [3 /*break*/, 6];
-                case 5:
-                    console.error('File location is undefined or empty.');
-                    process.exit(1);
-                    _a.label = 6;
-                case 6:
-                    //copy to filesystem
-                    try {
-                        jsonFilePath = "".concat(snykFilePath, "SnykReport.json");
-                        tl.writeFile(jsonFilePath, JSON.stringify(scanData));
-                        console.log("Wrote Snyk vulnerability data to ".concat(jsonFilePath, " "));
-                    }
-                    catch (err) {
-                        console.error("Failed to upload Snyk report to path: " + snykFilePath);
-                        console.log(err);
-                        process.exit(1);
-                    }
-                    _a.label = 7;
-                case 7:
-                    if (operation == "Process" || operation == "CopyAndProcess") {
-                        scanData = {};
-                        console.log("Proccesing Snyk data from: " + snykFilePath);
-                        try {
-                            scanData_1 = fs.readFileSync("".concat(snykFilePath, "SnykReport.json"), 'utf-8');
-                            scanData_1 = JSON.parse(scanData_1);
-                            console.log("Successfully retrieved scan data: " + JSON.stringify(scanData_1));
-                            //set properties in artifactory with scan data
-                            try {
-                                Artifactory.setProperties(scanData_1);
-                            }
-                            catch (err) {
-                                console.log("Error setting properties on artifact: " + err);
-                                process.exit(1);
-                            }
-                        }
-                        catch (err) {
-                            console.error("Error while attempting to retrieve scan data: " + err);
-                            process.exit(1);
-                        }
-                        //test
-                    }
-                    return [2 /*return*/];
+                }
             }
-        });
+            catch (err) {
+                console.log("Error retrieving Snyk report file: " + err);
+                process.exit(1);
+            }
+            //create scan data object
+            let scanData = {};
+            if (fileLocation) {
+                try {
+                    let codeJson = yield Utils.readFileContents(fileLocation); // Only call function if fileLocation is defined
+                    scanData = Utils.processCode(codeJson);
+                }
+                catch (err) {
+                    console.log("Error processing code results: " + err);
+                    process.exit(1);
+                }
+            }
+            else {
+                console.error('File location is undefined or empty.');
+                process.exit(1);
+            }
+            //copy to filesystem
+            try {
+                const jsonFilePath = `${snykFilePath}SnykReport.json`;
+                tl.writeFile(jsonFilePath, JSON.stringify(scanData));
+                console.log(`Wrote Snyk vulnerability data to ${jsonFilePath} `);
+            }
+            catch (err) {
+                console.error("Failed to upload Snyk report to path: " + snykFilePath);
+                console.log(err);
+                process.exit(1);
+            }
+        }
+        if (operation == "Process" || operation == "CopyAndProcess") {
+            //retrieve scan data
+            let scanData = {};
+            console.log("Proccesing Snyk data from: " + snykFilePath);
+            try {
+                let scanData = fs.readFileSync(`${snykFilePath}SnykReport.json`, 'utf-8');
+                scanData = JSON.parse(scanData);
+                console.log("Successfully retrieved scan data: " + JSON.stringify(scanData));
+                //set properties in artifactory with scan data
+                try {
+                    Artifactory.setProperties(scanData);
+                }
+                catch (err) {
+                    console.log("Error setting properties on artifact: " + err);
+                    process.exit(1);
+                }
+            }
+            catch (err) {
+                console.error("Error while attempting to retrieve scan data: " + err);
+                process.exit(1);
+            }
+            //test
+        }
     });
 }
 run();
